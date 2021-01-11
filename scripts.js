a1d17bfc921a3eba571bf53e68d99c972da0d780
Kingsley <kingsleyumujeyan@gmail.com>
2021-01-12 00:58:26 +0100

    ft--updated scripts and fixed errors

--- scripts.js
+++ scripts.js
@@ -7,38 +7,51 @@ const errorMessage = document.querySelector(".error");
 const searchForm = document.querySelector(".form");
 const searchResults = document.querySelector(".list");
 const nominateBtn = document.querySelector(".nominate");
+const query = document.querySelector(".search_query");
+const loader = document.querySelector(".loader");
 
 const apiKey = config.API_KEY;
 
 
 
-
+// Loader
+function showLoader() {
+    loader.className = "show";
+    setTimeout(() => {
+        loader.className = loader.className.replace("show", "");
+    }, 10);
+}
 // Search for Movies
 function searchMovies() {
     if(searchInput.value !== ""){
         searchInput.style.border = "2px solid #004c3f";
         errorMessage.style.display = "none";
+        query.innerHTML = `Results for "${searchInput.value}"`
         console.log(searchInput.value)
+        showLoader();
         let url = `http://www.omdbapi.com/?s=${searchInput.value}&apikey=${apiKey}`;
         fetch(url)
         .then(response => {
             if(response.ok){
                 return response.json()
             } else {
-                return Promise.reject(response)
+
+                return Promise.reject("Something went wrong",response)
             }
         })
         .then(data => {
-            const { Title, Year } = data;
-            console.log(data);
+            const movieResults = data.Search;
+            console.log(movieResults);
             const li = document.createElement("li");
             li.classList.add("result");
-            const markup = `
-                <span class="movie_title">${Title}</span>
-                <span class="year">${Year}</span>
+            movieResults.forEach(item => {
+                const markup = `
+                <span class="movie_title">${item.Title}</span>
+                <span class="year">${item.Year}</span>
                 <button class="nominate">Nominate</button>
             `;
-            li.innerHTML = markup;
+            li.innerHTML += markup;
+            });
             searchResults.appendChild(li);
         })
         .catch(error => console.warn(error));
@@ -48,7 +61,6 @@ function searchMovies() {
         errorMessage.style.display = "block";
     }
 }
-
 // Nominate Movie
 // Save movie
 // Remove Movie
