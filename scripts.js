94c08388d9bca92858a494956689fee310cc26d7
Kingsley <kingsleyumujeyan@gmail.com>
2021-01-17 01:12:44 +0100

    ft--updated list

--- scripts.js
+++ scripts.js
@@ -5,12 +5,26 @@ const searchBtn = document.querySelector(".searchBtn");
 const searchInput = document.querySelector(".search");
 const errorMessage = document.querySelector(".error");
 const searchForm = document.querySelector(".form");
-const searchResults = document.querySelector(".list");
-const nominateBtn = document.querySelector(".nominate");
-const query = document.querySelector(".search_query");
+const resultsContainer = document.querySelector(".results");
+
+const query = document.querySelector(".query");
 const loader = document.querySelector(".loader");
 
+
 const apiKey = config.API_KEY;
+const favorites = [];
+const results = [];
+
+document.querySelector("body").addEventListener("click", (e) => {
+    if(e.target.className.toLowerCase() === "nominate"){
+        console.log("clicked")
+        nominateMovie();
+    }
+})
+
+let nominateBtn = document.createElement("button");
+nominateBtn.setAttribute("class", "nominate");
+nominateBtn.innerHTML = "nominate";
 
 
 
@@ -26,7 +40,10 @@ function searchMovies() {
     if(searchInput.value !== ""){
         searchInput.style.border = "2px solid #004c3f";
         errorMessage.style.display = "none";
-        query.innerHTML = `Results for "${searchInput.value}"`
+        const queryMarkUp = `
+            <span class="search_query">Results for "${searchInput.value}"</span>
+        `
+        query.innerHTML = queryMarkUp;
         console.log(searchInput.value)
         showLoader();
         let url = `http://www.omdbapi.com/?s=${searchInput.value}&apikey=${apiKey}`;
@@ -35,24 +52,28 @@ function searchMovies() {
             if(response.ok){
                 return response.json()
             } else {
-
                 return Promise.reject("Something went wrong",response)
             }
         })
         .then(data => {
             const movieResults = data.Search;
-            console.log(movieResults);
-            const li = document.createElement("li");
-            li.classList.add("result");
-            movieResults.forEach(item => {
-                const markup = `
-                <span class="movie_title">${item.Title}</span>
+            results.push(movieResults);
+            localStorage.setItem("results",JSON.stringify(results))
+            console.log("movieList",results);
+            let ul = document.createElement("ul");
+            resultsContainer.appendChild(ul);
+            movieResults.map((item) => {
+                let li = document.createElement("li");
+                li.classList.add("result");
+                let markUp = `<span class="movie_title">${item.Title}</span>
                 <span class="year">${item.Year}</span>
-                <button class="nominate">Nominate</button>
-            `;
-            li.innerHTML += markup;
-            });
-            searchResults.appendChild(li);
+                <button class="nominate">Nominate</button>`;
+                
+                li.innerHTML += markUp;
+                ul.appendChild(li);
+            })
+            
+            
         })
         .catch(error => console.warn(error));
         searchInput.focus();
@@ -61,7 +82,30 @@ function searchMovies() {
         errorMessage.style.display = "block";
     }
 }
+
 // Nominate Movie
+function nominateMovie(imdbID) {
+    JSON.parse(localStorage.getItem("results"))
+    for (let i = 0; i < results.length; i++) {
+        let favoriteMovie = null;
+        for (let k = 0; k < favorites.length; k++) {
+            if(favorites[k].result.imdbID === results[i].imdbID){
+                favoriteMovie = favorites[k];
+                favorites[k];
+                break;
+            }
+        }
+        if(favoriteMovie === null){
+            let favoriteMovie = { result: results[i] };
+            favorites.push(favoriteMovie);
+            console.log(favorites)
+        }
+        
+    }
+    
+}
+
+
 // Save movie
 // Remove Movie
 
@@ -72,6 +116,10 @@ searchBtn.addEventListener("click", (e) => {
     searchMovies();
     searchForm.reset();
 })
+
+// Clear results
+
+
 
 
 
